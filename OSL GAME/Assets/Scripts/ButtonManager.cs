a8cab183a142806bda3988007626730746a6a4ac
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2024-01-04 21:14:59 -0500

    meta

--- OSL GAME/Assets/Scripts/ButtonManager.cs
+++ OSL GAME/Assets/Scripts/ButtonManager.cs
@@ -64,60 +64,68 @@ public class ButtonManager : MonoBehaviour
                     btext.text = role[i].name;
                 }else if (i > 17 && i <= 23)
                 {
-                Vector3 place = new Vector3((i - 3 * buttonfixoff) * 200, -200) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 3 * buttonfixoff) * 200, -200) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 23 && i <= 29)
                 {
-                Vector3 place = new Vector3((i - 4 * buttonfixoff) * 200,-250) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 4 * buttonfixoff) * 200,-250) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 29 && i <= 35)
                 {
-                Vector3 place = new Vector3((i - 5 * buttonfixoff) * 200, -300) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 5 * buttonfixoff) * 200, -300) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 35 && i <= 41)
                 {
-                Vector3 place = new Vector3((i - 6 * buttonfixoff) * 200, -350) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 6 * buttonfixoff) * 200, -350) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 41 && i <= 47)
                 {
-                Vector3 place = new Vector3((i - 7 * buttonfixoff) * 200, -400) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 7 * buttonfixoff) * 200, -400) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 47 && i <= 53)
                 {
-                Vector3 place = new Vector3((i - 8 * buttonfixoff) * 200, -450) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 8 * buttonfixoff) * 200, -450) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }else if (i > 53 && i <= 59)
                 {
-                Vector3 place = new Vector3((i - 9 * buttonfixoff) * 200, -500) + offset;
-                newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
-                RectTransform rect = (RectTransform)newbut.transform;
-                rect.anchoredPosition = place;
-                btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-                btext.text = role[i].name;
+                    Vector3 place = new Vector3((i - 9 * buttonfixoff) * 200, -500) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
+                }else if (i > 59 && i <= 65)
+                {
+                    Vector3 place = new Vector3((i - 10 * buttonfixoff) * 200, -550) + offset;
+                    newbut = Instantiate<GameObject>(button.gameObject, WhereYouWantButtonsParented);
+                    RectTransform rect = (RectTransform)newbut.transform;
+                    rect.anchoredPosition = place;
+                    btext = newbut.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+                    btext.text = role[i].name;
                 }
 
         }
